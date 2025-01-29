96a1a34918544899619ff260e113174b2f4aa95d
hakhan1999 <khanhassaan961@gmail.com>
2025-01-29 19:25:41 +0500

    Info Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Info from "./components/Info";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Hero/>
+      <Hero />
+      <Info />
     </>
   );
 };
---
+++ src/components/Info.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/Info.css'
+
+const Info = () => {
+  return (
+    <section className="container sec-padded info-sec">
+      <div className="wrapper">
+        <p>
+          Do you value a combination of humor and sophisticated technology?
+          Because I am. Go no farther—let's code something unforgettable
+          together if you want a digital realm that is all signal, no noise, and
+          a hint of dry humor.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Info;
